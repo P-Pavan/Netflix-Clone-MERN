6a86c0752c38eee0116e5bce08f4265489363f2e
P-Pavan <ppavan2002@gmail.com>
2023-04-18 20:10:24 +0530

    push

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -2,15 +2,23 @@ import "./list.scss"
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import ListItem from "../listItem/ListItem";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function List() {
     
+    const [slideNumber, setSlideNumber] = useState(0);
+
     const listRef = useRef()
 
     const handleClick = (direction) => {
-        if(direction === "left") {
-            listRef.current.style.transform = `translateX(230px)`
+        let distance = listRef.current.getBoundingClientRect().x - 50
+        if(direction === "left" && slideNumber>0) {
+            listRef.current.style.transform = `translateX(${230 + distance}px)`
+            setSlideNumber(slideNumber-1)
+        }
+        if(direction === "right" && slideNumber<5) {
+            listRef.current.style.transform = `translateX(${distance - 230}px)`
+            setSlideNumber(slideNumber+1)
         }
     }
 
@@ -19,7 +27,7 @@ function List() {
     <div className='list'>
         <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
-                <ArrowBackIosNewOutlinedIcon className="slideArrow left" onClick={handleClick("left")}/>
+                <ArrowBackIosNewOutlinedIcon className="slideArrow left" onClick={() => handleClick("left")}/>
                 <div className="container" ref={listRef}>
                 <ListItem />
                 <ListItem />
@@ -38,7 +46,7 @@ function List() {
                 <ListItem />
                 <ListItem />
                 </div>
-                <ArrowForwardIosOutlinedIcon className="slideArrow right" onClick={handleClick("right")}/>
+                <ArrowForwardIosOutlinedIcon className="slideArrow right" onClick={() => handleClick("right")}/>
             </div>
     </div>
   )
