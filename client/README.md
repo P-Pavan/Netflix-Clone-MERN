9db0db2492bd65b5cbe9ba7b09f52a6bf2f9c069
P-Pavan <ppavan2002@gmail.com>
2023-05-01 21:51:54 +0530

    alt

--- client/README.md
+++ client/README.md
@@ -1,3 +1,1 @@
 # React UI
-
-> https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png
--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -3,7 +3,7 @@ import "./listItem.scss"
 function ListItem() {
   return (
     <div className="listItem">
-        item
+        <img src="https://img1.ak.crunchyroll.com/i/spire3/693598935088048cd25e4a4fc7b5ceb41572256544_full.jpg" alt="" />
     </div>
   )
 }
